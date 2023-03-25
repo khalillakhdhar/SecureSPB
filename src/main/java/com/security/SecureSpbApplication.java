a2ab59df853ccacac08f4f18306f3e2e9dba6ded
khalil <khalillakhdharatc@gmail.com>
2023-03-25 11:05:30 +0100

    task

--- src/main/java/com/security/SecureSpbApplication.java
+++ src/main/java/com/security/SecureSpbApplication.java
@@ -1,13 +1,35 @@
 package com.security;
 
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SecureSpbApplication {
+import com.security.dao.TaskRepository;
+import com.security.model.Task;
 
+
+@SpringBootApplication
+public class SecureSpbApplication implements CommandLineRunner {
+@Autowired
+TaskRepository taskRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SecureSpbApplication.class, args);
 	}
 
-}
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Stream.of("T1","T2","T3").forEach(t->{
+			taskRepository.save(new Task(0, t));
+			});
+		taskRepository.findAll().forEach(t->{
+			System.out.println(t.getTaskName());
+			});
+			}
+	}
+
+
