f13d69e57d08008012f6bbe248c6704d68b2b260
khalil <khalillakhdharatc@gmail.com>
2023-03-25 12:19:09 +0100

    auth

--- src/main/java/com/security/configuration/SecurityConfig.java
+++ src/main/java/com/security/configuration/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.security.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
@@ -14,4 +16,11 @@ public class SecurityConfig {
 	auth.inMemoryAuthentication().withUser("admin").password("{noop}1234").roles("USER","ADMIN");
 	auth.inMemoryAuthentication().withUser("user").password("{noop}1234").roles("USER");
 	}
+	protected void configure(HttpSecurity http) throws Exception {
+		http.formLogin();
+		http.csrf().disable();
+		http.authorizeRequests().requestMatchers("/login/**","register/**");
+		http.authorizeRequests().requestMatchers(HttpMethod.POST,"/tasks/**").hasRole("ADMIN");
+		http.authorizeRequests().anyRequest().authenticated();
+		}
 }
